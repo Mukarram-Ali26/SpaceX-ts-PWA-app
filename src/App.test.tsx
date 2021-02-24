6eef91fe6aa6c46bef473c2d504729290583acfa
Mukarram-Ali26 <67237389+Mukarram-Ali26@users.noreply.github.com>
2021-02-24 10:24:54 +0500

    add testing file

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { shallow, ShallowWrapper } from "enzyme";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Render App", () => {
+  let AppWrapper: ShallowWrapper<
+    any,
+    Readonly<{}>,
+    React.Component<{}, {}, any>
+  >;
+  beforeEach(() => (AppWrapper = shallow(<App />)));
+  it("should render img", () => {
+    expect(AppWrapper.find("img").length).toEqual(1);
+  });
+  it("should render MissionContainer", () => {
+    expect(AppWrapper.find("MissionContainer").length).toEqual(1);
+  });
 });
